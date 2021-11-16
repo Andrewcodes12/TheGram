4e224deecf86c1f8cb65b18c1d9420a0a92ce034
Andrewcodes12 <AndrewFava@yahoo.com>
2021-11-16 12:55:43 -0500

    users model done

--- app/models/user.py
+++ app/models/user.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import backref
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -10,6 +11,18 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    profileImage=db.Column( db.String, default='http://clipart-library.com/new_gallery/280-2806732_png-file-svg-default-profile-picture-png.png',nullable=False)
+
+    likes=db.relationship('Like',cascade = "all,delete", backref='user')
+    comment=db.relationship('Comment',cascade = "all,delete", backref='user')
+    post=db.relationship('Post',cascade = "all,delete", backref='user')
+    following = db.relationship(
+    'User', lambda: following,
+    primaryjoin=lambda: User.id == following.c.user_id,
+    secondaryjoin=lambda: User.id == following.c.following_id,
+    backref='user',
+    cascade='all, delete'
+    )
 
     @property
     def password(self):
@@ -26,5 +39,15 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'profileImage':self.profileImage,
+            'following': [user.to_dict_following() for user in self.following]
+        }
+
+    def to_dict_following(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'profileImage':self.profileImage,
         }
