60a45342a6312236648ebc1304f7e3238a75d892
Andrewcodes12 <AndrewFava@yahoo.com>
2021-11-17 10:42:15 -0500

    git posts route completed

---
+++ app/api/post_routes.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import Blueprint, jsonify, request, Flask
+from flask_login import login_required, current_user
+from app.models import db, User, Post, Comment, Like
+from flask_sqlalchemy import SQLAlchemy
+
+
+post_routes = Blueprint('posts', __name__)
+
+
+@post_routes.route('/', methods=['GET'])
+# @login_required
+def get_posts():
+    """
+    Gets all posts
+    """
+    posts = Post.query.all()
+    return jsonify([post.to_dict() for post in posts])
