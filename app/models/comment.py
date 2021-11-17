c61c3ca0446c63ae29623a3b244c6e64145dd89d
Andrewcodes12 <AndrewFava@yahoo.com>
2021-11-16 21:57:26 -0500

    comments seeded git add .

--- app/models/comment.py
+++ app/models/comment.py
@@ -6,10 +6,10 @@ class Comment(db.Model):
 
     id = db.Column(db.Integer(), primary_key=True)
     body= db.Column(db.String(255), nullable=False)
-    userId = db.Column(db.Integer(),db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer(),db.ForeignKey('users.id'), nullable=False)
     postId = db.Column(db.Integer(), db.ForeignKey('posts.id'))
 
-
+    comment_post=db.relationship('User', backref='comments', lazy=True)
     # post_a_comment= db.relationship('Post', backref=db.backref('comment_post'),cascade = "all,delete")
     # user_a_comment= db.relationship('User', backref=db.backref('comment_user'),cascade = "all,delete")
 
@@ -17,6 +17,6 @@ class Comment(db.Model):
         return {
             'id': self.id,
             'body': self.body,
-            'userId': self.userId,
+            'userId': self.user_id,
             'postId': self.postId
         }
--- app/seeds/comments.py
+++ app/seeds/comments.py
@@ -5,94 +5,94 @@ now = datetime.now()
 
 
 def seed_comments():
-    comment1 = Comment(userId=2, postId=4, body='Looks great!',
+    comment1 = Comment(body='Looks great!',user_id=2, postId=4,
                   )
     db.session.add(comment1)
-    comment2 = Comment(userId=3, postId=4, body='OKAY !',
+    comment2 = Comment( body='OKAY !',user_id=3, postId=4
                   )
     db.session.add(comment2)
-    comment3 = Comment(userId=3, postId=4, body='THIS IS AWESOME',
+    comment3 = Comment(body='THIS IS AWESOME',user_id=3, postId=4,
                   )
     db.session.add(comment3)
-    comment4 = Comment(userId=4, postId=4, body='where can i get one',
+    comment4 = Comment(body='where can i get one',user_id=4, postId=4,
                   )
     db.session.add(comment4)
-    comment5 = Comment(userId=1, postId=7, body='Hello',
+    comment5 = Comment(body='Hello',user_id=1, postId=7,
                   )
     db.session.add(comment5)
-    comment6 = Comment(userId=11, postId=7, body='DO THE THING!',
+    comment6 = Comment(body='DO THE THING!',user_id=11, postId=7,
                   )
     db.session.add(comment6)
-    comment7 = Comment(userId=10, postId=7, body='umpalumpa',
+    comment7 = Comment(body='umpalumpa',user_id=10, postId=7,
                   )
     db.session.add(comment7)
-    comment8 = Comment(userId=11, postId=8, body='yes',
+    comment8 = Comment(body='yes',user_id=11, postId=8,
                   )
     db.session.add(comment8)
-    comment9 = Comment(userId=9, postId=9, body='We are trying to reach you regarding your cars extended warranty',
+    comment9 = Comment(body='We are trying to reach you regarding your cars extended warranty',user_id=9, postId=9,
                   )
     db.session.add(comment9)
-    comment10 = Comment(userId=5, postId=12, body='you talking to me?',
+    comment10 = Comment(body='you talking to me?',user_id=5, postId=12,
                    )
     db.session.add(comment10)
-    comment11 = Comment(userId=2, postId=1, body='let meet up soon',
+    comment11 = Comment( body='let meet up soon',user_id=2, postId=1,
                    )
     db.session.add(comment11)
-    comment12 = Comment(userId=2, postId=24, body='how have you been',
+    comment12 = Comment(body='how have you been',user_id=2, postId=24,
                    )
     db.session.add(comment12)
-    comment13 = Comment(userId=3, postId=23, body='cant believe its not butter',
+    comment13 = Comment(body='cant believe its not butter',user_id=3, postId=23,
                    )
     db.session.add(comment13)
-    comment14 = Comment(userId=6, postId=23, body='"YAHTZEEEE',
+    comment14 = Comment(body='"YAHTZEEEE',user_id=6, postId=23,
                    )
     db.session.add(comment14)
-    comment15 = Comment(userId=4, postId=9, body='its me',
+    comment15 = Comment(body='its me',user_id=4, postId=9,
                    )
     db.session.add(comment15)
-    comment16 = Comment(userId=1, postId=8, body='hey girl hey',
+    comment16 = Comment(body='hey girl hey',user_id=1, postId=8,
                    )
     db.session.add(comment16)
-    comment17 = Comment(userId=3, postId=3, body='yoooooo',
+    comment17 = Comment(body='yoooooo',user_id=3, postId=3,
                    )
     db.session.add(comment17)
-    comment18 = Comment(userId=5, postId=5, body='spectacular',
+    comment18 = Comment(body='spectacular',user_id=5, postId=5,
                    )
     db.session.add(comment18)
-    comment19 = Comment(userId=4, postId=1, body='wwjd',
+    comment19 = Comment( body='wwjd',user_id=4, postId=1,
                    )
     db.session.add(comment19)
-    comment20 = Comment(userId=7, postId=1, body='boooooogala booooooogala',
+    comment20 = Comment(body='boooooogala booooooogala',user_id=7, postId=1,
                    )
     db.session.add(comment20)
-    comment21 = Comment(userId=6, postId=1, body='its electric boogey ooogey oooogey',
+    comment21 = Comment(body='its electric boogey ooogey oooogey',user_id=6, postId=1,
                    )
     db.session.add(comment21)
-    comment22 = Comment(userId=8, postId=10, body='lorem ipsum',
+    comment22 = Comment(body='lorem ipsum',user_id=8, postId=10,
                    )
     db.session.add(comment22)
-    comment23 = Comment(userId=9, postId=12, body='can you see this?',
+    comment23 = Comment(body='can you see this?',user_id=9, postId=12,
                    )
     db.session.add(comment23)
-    comment24 = Comment(userId=10, postId=20, body='where are you?',
+    comment24 = Comment(body='where are you?',user_id=10, postId=20,
                    )
     db.session.add(comment24)
-    comment25 = Comment(userId=11, postId=2, body='are you in town?',
+    comment25 = Comment(body='are you in town?',user_id=11, postId=2,
                    )
     db.session.add(comment25)
-    comment26 = Comment(userId=3, postId=4, body='lets go out tonight',
+    comment26 = Comment(body='lets go out tonight',user_id=3, postId=4,
                    )
     db.session.add(comment26)
-    comment27 = Comment(userId=2, postId=7, body='astonishing',
+    comment27 = Comment(body='astonishing',user_id=2, postId=7,
                    )
     db.session.add(comment27)
-    comment28 = Comment(userId=1, postId=9, body='wow',
+    comment28 = Comment(body='wow',user_id=1, postId=9,
                    )
     db.session.add(comment28)
-    comment29 = Comment(userId=6, postId=8, body='woah',
+    comment29 = Comment(body='woah',user_id=6, postId=8,
                    )
     db.session.add(comment29)
-    comment30 = Comment(userId=7, postId=8, body='hold up!',
+    comment30 = Comment( body='hold up!',user_id=7, postId=8,
                    )
     db.session.add(comment30)
 
