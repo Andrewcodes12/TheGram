1a6357b443851fd17023a588052300526fa6c8b7
Andrewcodes12 <AndrewFava@yahoo.com>
2021-11-17 18:31:43 -0500

    comment reducer done

--- react-app/src/store/comment.js
+++ react-app/src/store/comment.js
@@ -31,4 +31,101 @@ const deleteOne = (comment) => ({
 })
 
 
+export const getComments = () => async (dispatch) => {
+    const response = await fetch("api/comments/");
+    if (response.ok) {
+      const comments = await response.json();
+      dispatch(load(comments));
+    }
+}
 
+export const getComment = (id) => async (dispatch) => {
+    const response = await fetch(`api/comments/${id}/`);
+    if (response.ok) {
+      const comment = await response.json();
+      dispatch(loadOne(comment));
+    }
+}
+
+export const addComment = (comment) => async (dispatch) => {
+    const response = await fetch("api/comments/new/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(comment)
+    });
+    if (response.ok) {
+      const newComment = await response.json();
+      dispatch(addOne(newComment));
+    }
+}
+
+
+export const updateComment = (comment) => async (dispatch) => {
+    const response = await fetch(`api/comments/${comment.id}/`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(comment)
+    });
+    if (response.ok) {
+      const updatedComment = await response.json();
+      dispatch(updateOne(updatedComment));
+    }
+}
+
+
+export const deleteComment = (comment) => async (dispatch) => {
+    const response = await fetch(`api/comments/${comment.id}/`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(comment)
+    });
+    if (response.ok) {
+      dispatch(deleteOne(comment));
+    }
+}
+
+
+const initialState = {
+    comments: [],
+    comment: {}
+}
+
+const commentReducer = (state=initialState, action) => {
+    switch(action.type){
+        case LOAD:
+            return {
+                ...state,
+                comments: action.payload
+            }
+        case LOAD_ONE:
+            return {
+                ...state,
+                comment: action.payload
+            }
+        case ADD_ONE:
+            return {
+                ...state,
+                comments: [...state.comments, action.payload]
+            }
+        case UPDATE_ONE:
+            return {
+                ...state,
+                comments: state.comments.map(comment => comment.id === action.payload.id ? action.payload : comment)
+            }
+        case DELETE_ONE:
+            return {
+                ...state,
+                comments: state.comments.filter(comment => comment.id !== action.payload.id)
+            }
+        default:
+            return state
+    }
+}
+
+export default commentReducer;
