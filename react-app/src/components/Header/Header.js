2b87ce5f397cccabb651c848cef2ba8bc6feb9ed
Andrewcodes12 <AndrewFava@yahoo.com>
2021-11-17 19:27:54 -0500

    navbar done

--- react-app/src/components/Header/Header.js
+++ react-app/src/components/Header/Header.js
@@ -5,33 +5,26 @@ import './header.css'
 const Header = () => {
 
     return (
-        <div className="navigation">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossOrigin="anonymous" />
+    <div className="navBar">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossOrigin="anonymous" />
     <div className="logo">
-<div>
+    <div>
     <NavLink to='/feed'>
       The Gram
     </NavLink>
     </div>
   </div>
-  <div className="navigation-search-container">
+  <div className="navSearch">
   <i className="fas fa-search"></i>
-    <input className="search-field" type="text" placeholder="Search"/>
-    <div className="search-container">
-      <div className="search-container-box">
-        <div className="search-results">
-        </div>
-      </div>
-    </div>
+    <input className="search" type="text" placeholder="Search"/>
   </div>
-  <div className="navigation-icons">
+  <div className="rightNav">
       <NavLink to='/feed'>
   <i className="fas fa-home" id="home"></i>
     </NavLink>
     <NavLink to='/post/new/'>
     <i className="fas fa-upload" id="upload"></i>
     </NavLink>
-
       <NavLink to='/profile'>
       <i className="far fa-user-circle" id="profile"></i>
         </NavLink>
