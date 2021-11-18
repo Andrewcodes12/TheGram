a0eb115fcff1c704e0140b2c6f707c6cdce582f2
Andrewcodes12 <AndrewFava@yahoo.com>
2021-11-17 19:58:05 -0500

    all data is sitting in redux state

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -46,7 +46,6 @@ function App() {
           <SplashPage />
         </Route>
         <ProtectedRoute path='/feed' exact={true} >
-      
           <Feed />
         </ProtectedRoute>
       </Switch>
--- react-app/src/components/Feed/Feed.js
+++ react-app/src/components/Feed/Feed.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux"
+
 import Header from '../Header/Header'
+
+import {getPosts} from '../../store/post'
+import {getComments} from '../../store/comment'
+import {getLikes} from '../../store/like'
+
 
 
 const Feed = () => {
+const post = useSelector(state => state.post)
+const comments = useSelector(state => state.comments)
+const likes = useSelector(state => state.likes)
 
+const dispatch = useDispatch()
+
+
+useEffect(() => {
+    dispatch(getPosts())
+    dispatch(getComments())
+    dispatch(getLikes())
+}, [dispatch])
 
 
     return (
