87bb06f659085f010cd5dd9284ae1c98925ff9d9
Andrewcodes12 <84250366+Andrewcodes12@users.noreply.github.com>
2021-11-29 13:25:51 -0500

    minor changes

--- react-app/src/components/NewPost/NewPost.js
+++ react-app/src/components/NewPost/NewPost.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react'
 import { useDispatch,useSelector } from 'react-redux'
 import { useHistory } from 'react-router'
 import { addPost } from '../../store/post'
+import Header from '../Header/Header'
 
 import './newPost.css'
 
@@ -59,7 +60,8 @@ const handleSubmit = (e) => {
 
 
     return (
-    <div className="inputBox">
+      <div className="inputBox">
+      <Header />
       <h1>Create A Post</h1>
       <form onSubmit={handleSubmit}>
       <ul className="errors">
